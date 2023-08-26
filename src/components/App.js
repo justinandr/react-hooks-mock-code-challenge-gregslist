ff9cb79a0c3d702c00ef395645a4dff5661e106d
Justin Rodriguez <justin.r@me.com>
2023-08-26 15:25:14 -0400

    complete mock code challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then(res => res.json())
+    .then(data => setListings(data))
+  }, [])
+
+  function handleDelete(id){
+    const updatedListings = listings.filter(listing => listing.id !== id)
+
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(() => setListings(updatedListings))
+  }
+
+  function handleSearchSubmit(search) {
+    const listingsToDisplay = listings.filter(listing => listing.description.includes(search))
+    setListings(listingsToDisplay)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearchSubmit} />
+      <ListingsContainer onDelete={handleDelete} listings={listings} />
     </div>
   );
 }
